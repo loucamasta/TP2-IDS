ecb8a993a07ca53f4b0c0c67adede56eca0aa562
lourdes camasta <lcamasta@fi.uba.ar>
2025-09-28 12:55:31 -0300

    Agrego diccionario info_evento

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
+info_evento = {
+        "nombre": "Rally MTB 2025",
+        "organizador": "Club Social y Deportivo Unidos por el Deporte",
+        "descripcion": (
+        "El evento se desarrollará en la ciudad de Tandil, Buenos Aires, organizado por nuestro club, "
+        "el día 24 de Octubre de 2025 a las 8:00 AM. Existen dos modalidades corta y larga, "
+        "la primera es de 30km y la segunda de 80km."
+        ),
+        "fecha": "24 de Octubre de 2025",
+        "horario": "8:00 AM",
+        "lugar": "Tandil, Buenos Aires",
+        "tipo_carrera": "MTB rural",
+        "modalidad_costo": {
+            1: {"nombre": "Corta", "valor": "100"},
+            2: {"nombre": "Larga", "valor": "200"}
+        },
+        "auspiciantes": ["Ausp1", "Ausp2", "Ausp3"]
+}
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", info_evento=info_evento)
 
 @app.route("/registration")
 def registration():
